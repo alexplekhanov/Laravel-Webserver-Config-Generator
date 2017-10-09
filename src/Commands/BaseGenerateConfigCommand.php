0038af025cacc1d4aa262f9669f4853ab7b4318b
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2017-10-09 19:54:59 +0000

    Make some starting work, start implementing GenerateNginxConfigCommand

---
+++ src/Commands/BaseGenerateConfigCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Plekhanov\LaravelWebserverConfigGenerator\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+abstract class BaseGenerateConfigCommand extends Command
+{
+    /**
+     * Get output config filename.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @return string
+     */
+    protected function getOutputFilename(InputInterface $input)
+    {
+        $outputOption = $input->getOption('output');
+        
+        if (!is_null($outputOption)) {
+            return $outputOption;
+        }
+        
+        $currentWorkingDirectory = getcwd();
+        
+        if ($currentWorkingDirectory === false) {
+            throw new RuntimeException('Unknown issue with filesystem.');
+        }
+        
+        return $currentWorkingDirectory . $this->filename;
+    }
+    
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param  string  $outputFile
+     * @return void
+     */
+    protected function verifyOutputFileDoesntExist($outputFile)
+    {
+        if (is_dir($outputFile) || is_file($outputFile)) {
+            throw new RuntimeException("File [{$outputFile}] already exists!");
+        }
+    }
+}
---
+++ src/Commands/GenerateNginxConfigCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Plekhanov\LaravelWebserverConfigGenerator\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use RuntimeException;
+
+class GenerateNginxConfigCommand extends BaseGenerateConfigCommand
+{
+    protected $filename = 'nginx.conf';
+    
+    protected function configure()
+    {
+         $this
+            // the name of the command (the part after "bin/console")
+            ->setName('generate:nginx')
+    
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Generate config for Nginx webserver')
+    
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to generate Nginx webserver for Laravel project.')
+            
+            ->addOption('output', 'o', InputOption::VALUE_REQUIRED, 'Output file')
+            
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Forces generation even if the output file already exists')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output = $this->getOutputFilename($input);
+        
+        // check for existence of output file
+        if (!$input->getOption('force')) {
+            $this->verifyOutputFileDoesntExist($output);
+        }
+    }
+}
